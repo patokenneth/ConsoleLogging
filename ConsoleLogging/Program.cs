de564dd973e12730bb0f888df0fb7b8c2a007dd9
Patokenneth <chiemezieoloto4@gmail.com>
2021-07-11 10:46:20 +0100

    Add project files.

---
+++ ConsoleLogging/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using System;
+
+namespace ConsoleLogging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var collections = new ServiceCollection();
+            ServiceProvider provider = ConfigureServices(collections);
+
+            ILogger log = provider.GetService<ILogger<Program>>();
+            log.LogInformation("Hello guys!");
+           
+        }
+
+        private static ServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging(configure => { configure.AddConsole(); });
+            return services.BuildServiceProvider();
+        }
+    }
+}
